917aa390283bcb2fc9d0fdf70a55cc5a6ea1dc4f
Fei20160811 <paradox0912@gmail.com>
2020-05-29 16:46:39 +0800

    取得網址參數撈取相關資料

--- mainsite/views.py
+++ mainsite/views.py
@@ -1,6 +1,6 @@
 from django.template.loader import get_template
 from datetime import datetime
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Post
 
@@ -11,3 +11,14 @@ def homepage(request):
     now = datetime.now()
     html = template.render(locals())
     return HttpResponse(html)
+
+def showpost(request, id):
+    template = get_template("post.html")
+    try:
+        post = Post.objects.get(id=id)
+        if post:
+            now = datetime.now()
+            html = template.render(locals())
+            return HttpResponse(html)
+    except:
+        return redirect("/")
