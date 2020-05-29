c6cfa0c3910d9f33726243b08a48843c422dca78
Fei20160811 <paradox0912@gmail.com>
2020-05-29 14:45:49 +0800

    新增資料與改變後台文章列表顯示樣式

--- mainsite/models.py
+++ mainsite/models.py
@@ -3,9 +3,16 @@ from django.utils import timezone
 
 # Create your models here.
 class Post(models.Model):
+    id = models.AutoField(primary_key=True)
     question = models.CharField(max_length=300)
-    seasonCard = models.CharField(max_length=20)
-    symbolCard = models.CharField(max_length=20)
-    travelCard = models.CharField(max_length=20)
-    explain = models.TextField()
+    algorithm = models.CharField(max_length=100)
+    desc = models.TextField()
+    answer = models.TextField()
+    note = models.CharField(max_length=200)
     pubDate = models.DateTimeField(default=timezone.now)
+
+    class Meat:
+        ordering = ('-pubDate',)
+        
+    def __str__(self): #可用物件實體名稱表示一段字串
+        return self.question
